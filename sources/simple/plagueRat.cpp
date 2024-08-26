6c6feb3877fb285f05ea2a1834c9be4d8bc69800
Tomas Maly <malytomas@ucpu.cz>
2024-08-26 10:36:59 +0200

    plague rat (test custom texture)

---
+++ sources/simple/plagueRat.cpp
@@ -0,0 +1,89 @@
+#include <cage-core/assetManager.h>
+#include <cage-core/entities.h>
+#include <cage-core/hashString.h>
+#include <cage-core/logger.h>
+#include <cage-engine/highPerformanceGpuHint.h>
+#include <cage-engine/scene.h>
+#include <cage-engine/window.h>
+#include <cage-simple/engine.h>
+#include <cage-simple/fpsCamera.h>
+#include <cage-simple/statisticsGui.h>
+
+using namespace cage;
+constexpr uint32 AssetsName = HashString("cage-tests/plagueRat/rat.pack");
+
+int main(int argc, char *args[])
+{
+	try
+	{
+		// log to console
+		Holder<Logger> log1 = newLogger();
+		log1->format.bind<logFormatConsole>();
+		log1->output.bind<logOutputStdOut>();
+
+		engineInitialize(EngineCreateConfig());
+
+		// events
+		//const auto updateListener = controlThread().update.listen(&update);
+		const auto closeListener = engineWindow()->events.listen(inputFilter([](input::WindowClose) { engineStop(); }));
+
+		// window
+		engineWindow()->setMaximized();
+		engineWindow()->title("plague rat (test custom texture)");
+
+		// entities
+		EntityManager *ents = engineEntities();
+		{ // camera
+			Entity *e = ents->create(1);
+			e->value<TransformComponent>().position = Vec3(0, 3, 7);
+			e->value<TransformComponent>().orientation = Quat(Degs(-20), Degs(), Degs());
+			CameraComponent &c = e->value<CameraComponent>();
+			c.near = 0.1;
+			c.far = 1000;
+			c.ambientColor = Vec3(1);
+			c.ambientIntensity = 0.2;
+		}
+		{ // sun
+			Entity *e = ents->createUnique();
+			TransformComponent &t = e->value<TransformComponent>();
+			t.orientation = Quat(Degs(-50), Degs(270), Degs());
+			t.position = Vec3(0, 2, 0);
+			LightComponent &l = e->value<LightComponent>();
+			l.lightType = LightTypeEnum::Directional;
+			l.color = Vec3(1);
+			l.intensity = 1;
+			ShadowmapComponent &s = e->value<ShadowmapComponent>();
+			s.resolution = 2048;
+			s.directionalWorldSize = 15;
+		}
+		{ // floor
+			Entity *e = ents->createUnique();
+			e->value<TransformComponent>().position = Vec3();
+			e->value<RenderComponent>().object = HashString("cage-tests/skeletons/floor/floor.obj");
+		}
+		{ // the rat
+			Entity *e = ents->createAnonymous();
+			e->value<TransformComponent>().position = Vec3(0, 0, 0);
+			e->value<TransformComponent>().orientation = Quat({}, Degs(180), {});
+			e->value<RenderComponent>().object = HashString("cage-tests/plagueRat/Rat.object");
+			e->value<SkeletalAnimationComponent>().name = HashString("cage-tests/plagueRat/Rat.glb?Rat_Walk");
+		}
+
+		Holder<FpsCamera> cameraCtrl = newFpsCamera(ents->get(1));
+		cameraCtrl->mouseButton = MouseButtonsFlags::Left;
+		cameraCtrl->movementSpeed = 0.3;
+		Holder<StatisticsGui> statistics = newStatisticsGui();
+
+		engineAssets()->load(AssetsName);
+		engineRun();
+		engineAssets()->unload(AssetsName);
+		engineFinalize();
+
+		return 0;
+	}
+	catch (...)
+	{
+		detail::logCurrentCaughtException();
+		return 1;
+	}
+}
