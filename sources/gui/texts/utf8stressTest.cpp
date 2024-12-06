e00eb5fb1fabcb873a5b63c287999caaeb440aa3
Tomas Maly <malytomas@ucpu.cz>
2024-12-06 01:25:31 +0100

    utf8 test; multilingual quotes

---
+++ sources/gui/texts/utf8stressTest.cpp
@@ -0,0 +1,26 @@
+#include "../gui.h"
+
+#include <cage-core/lineReader.h>
+
+using namespace cage;
+
+PointerRange<const uint8> utf8stressTestTxt();
+
+class GuiTestImpl : public GuiTestClass
+{
+public:
+	void initialize() override
+	{
+		auto g = newGuiBuilder(engineGuiEntities());
+		auto _1 = g->panel();
+		auto _2 = g->scrollbars();
+		auto _3 = g->alignment();
+		auto _4 = g->column();
+		const auto lr = newLineReader(utf8stressTestTxt().cast<const char>());
+		String s;
+		while (lr->readLine(s))
+			g->label().text(s).textFormat(GuiTextFormatComponent{ .font = HashString("cage/font/ubuntu/monospace.ttf") });
+	}
+};
+
+MAIN(GuiTestImpl, "utf8 stress test")
