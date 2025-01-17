9f10fa0e97555a24fa642cef10035bfe49ec8d3c
Tomas Maly <malytomas@ucpu.cz>
2025-01-17 10:10:02 +0100

    update timingFrames

--- sources/simple/timingFrames.cpp
+++ sources/simple/timingFrames.cpp
@@ -1,20 +1,22 @@
+#include <cage-core/assetsManager.h>
 #include <cage-core/concurrent.h>
 #include <cage-core/entities.h>
 #include <cage-core/entitiesVisitor.h>
 #include <cage-core/hashString.h>
 #include <cage-core/logger.h>
+#include <cage-core/profiling.h>
 #include <cage-core/string.h>
-#include <cage-engine/guiComponents.h>
+#include <cage-engine/guiBuilder.h>
 #include <cage-engine/guiManager.h>
 #include <cage-engine/highPerformanceGpuHint.h>
 #include <cage-engine/scene.h>
-#include <cage-engine/sceneScreenSpaceEffects.h>
 #include <cage-engine/window.h>
 #include <cage-simple/engine.h>
-#include <cage-simple/fpsCamera.h>
 #include <cage-simple/statisticsGui.h>
 
 using namespace cage;
+
+constexpr uint32 AssetsName = HashString("cage-tests/music/music.pack");
 
 uint64 updateDelay = 0;
 uint64 prepareDelay = 0;
@@ -40,32 +42,36 @@ void controlInit()
 {
 	EntityManager *ents = engineEntities();
 	ents->defineComponent(ShotComponent());
+
 	{ // camera
 		Entity *e = ents->create(1);
 		e->value<TransformComponent>();
 		CameraComponent &c = e->value<CameraComponent>();
 		c.ambientColor = Vec3(1);
 		c.ambientIntensity = 1;
-		e->value<ScreenSpaceEffectsComponent>();
+		e->value<ListenerComponent>();
 	}
-	{ // box 1
+
+	{ // box
 		Entity *e = ents->create(2);
 		e->value<TransformComponent>();
 		e->value<RenderComponent>().object = HashString("cage/model/fake.obj");
+		e->value<SoundComponent>().name = HashString("cage-tests/music/PurplePlanet/RetroGamer.mp3");
+		e->value<SoundComponent>().loop = true;
 	}
 }
-
-void guiUpdate();
 
 void update()
 {
 	const uint64 time = engineControlTime();
 	EntityManager *ents = engineEntities();
-	{ // box 1
+
+	{ // box
 		Entity *e = ents->get(2);
 		TransformComponent &t = e->value<TransformComponent>();
 		t.position = Vec3(sin(Rads(time * 1e-6)) * 10, cos(Rads(time * 1e-6)) * 10, -20);
 	}
+
 	entitiesVisitor(
 		[&](Entity *e, TransformComponent &t, ShotComponent &s)
 		{
@@ -76,15 +82,24 @@ void update()
 		},
 		ents, true);
 	shoot(Transform(Vec3(-20, 0, -25), Quat(Degs(), Degs(-90), Degs())));
-	if (updateDelay)
+
+	{
+		const ProfilingScope profiling("update delay");
 		threadSleep(updateDelay);
-	guiUpdate();
+	}
+
+	{
+		const ProfilingScope profiling("manual delay");
+		if (engineWindow()->keyboardKey(32))
+			threadSleep(1'000'000);
+	}
 }
 
 void prepare()
 {
 	if (!prepareDelay)
 		return;
+	const ProfilingScope profiling("prepare delay");
 	threadSleep(prepareDelay);
 }
 
@@ -92,6 +107,7 @@ void render()
 {
 	if (!renderDelay)
 		return;
+	const ProfilingScope profiling("render delay");
 	threadSleep(renderDelay);
 }
 
@@ -99,113 +115,73 @@ void soundUpdate()
 {
 	if (!soundDelay)
 		return;
+	const ProfilingScope profiling("sound delay");
 	threadSleep(soundDelay);
 }
 
 void guiInit()
 {
-	GuiManager *g = cage::engineGuiManager();
+	Holder<GuiBuilder> g = newGuiBuilder(engineGuiEntities());
+	auto _1 = g->alignment(Vec2(0));
+	auto _2 = g->panel();
+	auto _3 = g->scrollbars();
+	auto _4 = g->verticalTable(2);
 
-	Entity *panel = g->entities()->createUnique();
-	{
-		panel->value<GuiLayoutScrollbarsComponent>();
-		panel->value<GuiLayoutAlignmentComponent>().alignment = Vec2(0);
-	}
-
-	Entity *layout = g->entities()->createUnique();
-	{
-		layout->value<GuiPanelComponent>();
-		layout->value<GuiLayoutTableComponent>();
-		GuiParentComponent &child = layout->value<GuiParentComponent>();
-		child.parent = panel->id();
-	}
-
-	// controls
 	static constexpr const char *names[] = { "control tick", "control delay", "prepare delay", "dispatch delay", "sound delay" };
 	static constexpr const uint64 values[] = { 1000 / 20, 0, 0, 0, 0 };
-	static_assert(sizeof(names) / sizeof(names[0]) == sizeof(values) / sizeof(values[0]), "arrays must have same length");
-	for (uint32 i = 0; i < sizeof(names) / sizeof(names[0]); i++)
+	static_assert(array_size(names) == array_size(values));
+	for (uint32 i = 0; i < array_size(names); i++)
 	{
-		Entity *lab = g->entities()->createUnique();
-		{
-			GuiParentComponent &child = lab->value<GuiParentComponent>();
-			child.parent = layout->id();
-			child.order = i * 2 + 0;
-			lab->value<GuiLabelComponent>();
-			GuiTextComponent &t = lab->value<GuiTextComponent>();
-			t.value = names[i];
-		}
-		Entity *con = g->entities()->create(20 + i);
-		{
-			GuiParentComponent &child = con->value<GuiParentComponent>();
-			child.parent = layout->id();
-			child.order = i * 2 + 1;
-			GuiInputComponent &c = con->value<GuiInputComponent>();
-			c.type = InputTypeEnum::Integer;
-			c.min.i = i >= 1 ? 0 : 1;
-			c.max.i = 1000;
-			c.step.i = 1;
-			c.value = Stringizer() + values[i];
-		}
+		g->label().text(names[i]);
+		g->input(values[i], i >= 1 ? 0 : 1, 1000)
+			.event(inputFilter(
+				[i](input::GuiValue v)
+				{
+					const GuiInputComponent &t = v.entity->value<GuiInputComponent>();
+					if (!t.valid)
+						return;
+					CAGE_ASSERT(isDigitsOnly(t.value) && !t.value.empty());
+					if (i == 0)
+					{
+						controlThread().updatePeriod(toUint32(t.value) * 1000);
+					}
+					else
+					{
+						static uint64 *data[] = { nullptr, &updateDelay, &prepareDelay, &renderDelay, &soundDelay };
+						static_assert(array_size(data) == array_size(values));
+						*(data[i]) = toUint32(t.value) * 1000;
+					}
+				}));
 	}
-}
-
-namespace
-{
-	void setIntValue(uint32 index, uint64 &value)
-	{
-		Entity *control = cage::engineGuiEntities()->get(20 + index);
-		const GuiInputComponent &t = control->value<GuiInputComponent>();
-		if (t.valid)
-		{
-			CAGE_ASSERT(isDigitsOnly(t.value) && !t.value.empty());
-			value = toUint32(t.value) * 1000;
-		}
-	}
-}
-
-void guiUpdate()
-{
-	{
-		Entity *control = cage::engineGuiEntities()->get(20 + 0);
-		const GuiInputComponent &t = control->value<GuiInputComponent>();
-		if (t.valid)
-		{
-			CAGE_ASSERT(isDigitsOnly(t.value) && !t.value.empty());
-			controlThread().updatePeriod(toUint32(t.value) * 1000);
-		}
-	}
-	setIntValue(1, updateDelay);
-	setIntValue(2, prepareDelay);
-	setIntValue(3, renderDelay);
-	setIntValue(4, soundDelay);
 }
 
 int main(int argc, char *args[])
 {
 	try
 	{
-		// log to console
-		Holder<Logger> log1 = newLogger();
-		log1->format.bind<logFormatConsole>();
-		log1->output.bind<logOutputStdOut>();
-
+		initializeConsoleLogger();
 		engineInitialize(EngineCreateConfig());
 
 		// events
-		const auto updateListener = controlThread().update.listen(&update);
-		const auto prepareListener = graphicsPrepareThread().prepare.listen(&prepare);
-		const auto renderListener = graphicsDispatchThread().dispatch.listen(&render);
-		const auto soundListener = soundThread().sound.listen(&soundUpdate);
-		const auto guiInitListener = controlThread().initialize.listen(&guiInit);
+		const auto updateListener = controlThread().update.listen(update);
+		const auto prepareListener = graphicsPrepareThread().prepare.listen(prepare);
+		const auto renderListener = graphicsDispatchThread().dispatch.listen(render);
+		const auto soundListener = soundThread().sound.listen(soundUpdate);
+		const auto guiInitListener = controlThread().initialize.listen(guiInit);
 		const auto closeListener = engineWindow()->events.listen(inputFilter([](input::WindowClose) { engineStop(); }));
 
 		engineWindow()->setMaximized();
 		engineWindow()->title("timing frames");
 		controlInit();
-		Holder<StatisticsGui> statistics = newStatisticsGui();
+		engineAssets()->load(AssetsName);
 
-		engineRun();
+		{
+			Holder<StatisticsGui> statistics = newStatisticsGui();
+			statistics->statisticsMode = StatisticsGuiModeEnum::Latest;
+			engineRun();
+		}
+
+		engineAssets()->unload(AssetsName);
 		engineFinalize();
 
 		return 0;
